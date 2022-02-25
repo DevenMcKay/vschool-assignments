d58306e2b21ca1ef07caffe964b37297ca0fda9a
Deven McKay <deven_mckay@yahoo.com>
2022-02-25 15:07:51 -0500

    Revert "Revert "level 5 bounty hunter start""
    This reverts commit 585dd47b28cc253263a8a187c53718314ffddd9a.

---
+++ level5/practice/modules/modules/reverse string/main.js
@@ -0,0 +1,4 @@
+const app = require("./string-reverse")
+
+console.log(app("apple"))
+console.log(app)
---
+++ level5/practice/modules/modules/reverse string/string-reverse.js
@@ -0,0 +1,5 @@
+ const reverse = function (string) {
+   return string.split("").reverse().join("")
+ }
+
+ module.exports = reverse
