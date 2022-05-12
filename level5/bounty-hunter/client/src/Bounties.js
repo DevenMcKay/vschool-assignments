1a09dc4576f3f57409afffd52080e74bbfb9a443
Deven McKay <deven_mckay@yahoo.com>
2022-05-11 22:50:45 -0400

    updated routes for atlas, heroku, and build

--- level5/bounty-hunter/client/src/Bounties.js
+++ level5/bounty-hunter/client/src/Bounties.js
@@ -7,7 +7,7 @@ function Bounties() {
   const [bountyList, setBountyList] = useState()
 
   function getBounties() {
-    axios.get("/bounties")
+    axios.get("https://sw-bounty-huner.herokuapp.com/bounties")
       .then(res => setBountyList(res.data))
       .catch(err => console.log(err))
   }
@@ -17,20 +17,20 @@ function Bounties() {
   }, [])
 
   function addBounty(newBounty) {
-    axios.post("/bounties", newBounty)
+    axios.post("https://sw-bounty-huner.herokuapp.com/bounties", newBounty)
       .then(res => setBountyList(prevList => [...prevList, res.data]))
       .catch(err => console.log(err))
   }
 
   function deleteBounty(bountyId) {
-    axios.delete(`/bounties/${bountyId}`)
+    axios.delete(`https://sw-bounty-huner.herokuapp.com/bounties/${bountyId}`)
       .then(res =>
         setBountyList(prevList => prevList.filter(item => item._id !== bountyId)))
       .catch(err => console.log(err))
   }
 
   function editBounty(updates, bountyId) {
-    axios.put(`/bounties/${bountyId}`, updates)
+    axios.put(`https://sw-bounty-huner.herokuapp.com/bounties/${bountyId}`, updates)
       .then(res => {
         setBountyList(prevList => prevList.map(item => item._id !== bountyId ? item : res.data))
       })
