04a3de9bed3a49c74723bdcb22c82968e9cda411
Deven McKay <deven_mckay@yahoo.com>
2022-07-11 12:51:42 -0400

    Fixed message themes in from submit value in dom olympics

--- Level1/dom-man-olympics/dom-olympics-start/index.js
+++ Level1/dom-man-olympics/dom-olympics-start/index.js
@@ -2,9 +2,11 @@
 let myHeader = document.getElementById("header")
 myHeader.innerHTML += "<div class='header'><h1>JavaScript Made This!!</h1><h2><span class='name'>Deven </span>wrote the JavaScript</h2></div>"
 
+// User Message Input 
+document.querySelector("input").value = "Thanks! Goodbye"
 // Change Messages
 let topLeftMessage = document.getElementsByClassName("message left")[0]
-topLeftMessage.textContent = "What type of sandel does a frog wear?"
+topLeftMessage.textContent = "What type of sandal does a frog wear?"
 
 let bottomLeftMessage = document.getElementsByClassName("message left")[1]
 bottomLeftMessage.textContent = "Open-toad!"
@@ -16,10 +18,10 @@ let bottomRightMessage = document.getElementsByClassName("message right")[1]
 bottomRightMessage.textContent = "LOL"
 
 // Clear all messages 
-let allMessages = document.getElementsByClassName("messages")[0]
+let userReply = document.getElementsByClassName("messages")[0]
 
 function clearAllMessages(){
-  allMessages.textContent = "";
+  userReply.textContent = "";
 }
 
 let clearBtn = document.getElementById("clear-button")
@@ -31,55 +33,45 @@ let leftMessages = document.getElementsByClassName("message left")
 
 let rightMessages = document.getElementsByClassName("message right")
 
-document.querySelector("#theme-drop-down").addEventListener("change", function() {
-    if (this.value === "theme-one") {
-      for (let i = 0; i < leftMessages.length; i++){
-      leftMessages[i].style.backgroundColor = "burlywood"}
-      for (let i = 0; i < rightMessages.length; i++){
-      rightMessages[i].style.backgroundColor = "lightblue"
-      allMessages.style.color = "black"}
-    } else if (this.value === "theme-two") {
-      for (let i = 0; i < leftMessages.length; i++){
-      leftMessages[i].style.backgroundColor = "red"}
-      for (let i = 0; i < rightMessages.length; i++){
-      rightMessages[i].style.backgroundColor = "black"
-      allMessages.style.color = "white"}
-      }
-    }
-);
+let themeDropdown = document.querySelector("#theme-drop-down")
 
-// ------------- Inefficient (Doesn't change new posts) -----------------
-//
-// let theme = document.querySelector("#theme-drop-down").value
-// document.querySelector("#theme-drop-down").addEventListener("change", function() {
-//     if (this.value === "theme-one") {
-//       topLeftMessage.style.backgroundColor = "burlywood"
-//       bottomLeftMessage.style.backgroundColor = "burlywood"
-//       topRightMessage.style.backgroundColor = "lightblue"
-//       bottomRightMessage.style.backgroundColor = "lightblue"
-//       allMessages.style.color = "black"
-//     } else if (this.value === "theme-two"){
-//       topLeftMessage.style.backgroundColor = "red"
-//       bottomLeftMessage.style.backgroundColor = "red"
-//       topRightMessage.style.backgroundColor = "black"
-//       bottomRightMessage.style.backgroundColor = "black" 
-//       allMessages.style.color = "white"
-//     }
-// });
+themeDropdown.addEventListener("change", function() {
+  messageStyle(),
+  console.log('✅', themeDropdown)}
+)
+
+function messageStyle() {
+  if (themeDropdown.value === "theme-one") {
+    for (let i = 0; i < leftMessages.length; i++){
+    leftMessages[i].style.backgroundColor = "burlywood"}
+    for (let i = 0; i < rightMessages.length; i++){
+    rightMessages[i].style.backgroundColor = "lightblue"
+    userReply.style.color = "black"}
+  } else if (themeDropdown.value === "theme-two") {
+    for (let i = 0; i < leftMessages.length; i++){
+    leftMessages[i].style.backgroundColor = "red"}
+    for (let i = 0; i < rightMessages.length; i++){
+    rightMessages[i].style.backgroundColor = "black"
+    userReply.style.color = "white"}
+    }
+  }
+
 
 // More Messages
-let reply = document.querySelector("input").value
 let i = 0
 document.querySelector("form > button").addEventListener("click", function(event) { 
+  event.preventDefault()
+  let reply = document.querySelector("input").value
   if (i % 2 === 0){
     i++
-  event.preventDefault()
-  allMessages.innerHTML += `<p class="message left">${reply}</p>`
+  userReply.innerHTML += `<p class="message left">${reply}</p>`
   } else if (i % 2 !== 0){
     i++
-    event.preventDefault()
-    allMessages.innerHTML += `<p class="message right">${reply}</p>`
+    userReply.innerHTML += `<p class="message right">${reply}</p>`
   }
+  //  messageStyle()
+  document.querySelector("input").value = ""
+  messageStyle()
 })
 
 
